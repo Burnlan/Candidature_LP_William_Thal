73b3dab5e02007aec34bf82985a1adc4696d66d8
William <william.thal.67@gmail.com>
2022-02-16 12:31:58 +0100

    added db connection to server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 //we import our modules
 const express = require("express");
+const mongoose = require("mongoose");
+const db = require("./db");
 
 //init app using express
 const app = express();
@@ -7,6 +9,10 @@ const app = express();
 //allows use of JSON with express
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
+
+//we connect to the mongodb database
+mongoose.connect(db)
+    .then(() => console.log("connected to the database"));
 
 const server = app.listen(8666, function(){
     console.log("listening to requests on port 8666");
